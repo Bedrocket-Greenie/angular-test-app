b280d4a59e2a8c5b93ccf46df78e1f8d2c7b9af1
Nick Greenfield <pair+nick@pivotallabs.com>
2013-05-13 17:13:56 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularTest::Application.config.secret_token = '58bbb2dbbc899148ebf7897321720198788d8eaef37782da47bcd33e8eaf48bf6f7847d4759a92cb044065c081dca42fe8aa3ee18e925d9ea463e128479022bc'
